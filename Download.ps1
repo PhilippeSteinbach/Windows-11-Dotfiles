cf06320323c9414725d2b951e1b0005c0973c806
Philippe Steinbach <steinbach.phil@gmail.com>
2024-07-06 22:29:25 +0200

    logic changes in download script

--- Download.ps1
+++ Download.ps1
@@ -25,31 +25,53 @@ $DownloadResult = $FALSE
 # Check if Dotfiles folder exists and is not empty
 if (Test-Path $DotfilesFolder) {
   if ((Get-ChildItem -Path $DotfilesFolder).Count -gt 0) {
-    $UserConfirmation = Read-Host "Der Dotfiles-Ordner existiert bereits und ist nicht leer. Möchten Sie ihn überschreiben und fortfahren? (J/N)"
-    if ($UserConfirmation -ne 'J') {
-      Write-Host "Vorgang durch den Benutzer abgebrochen." -ForegroundColor Red
-      exit
-    } else {
-      Remove-Item -Path $DotfilesFolder -Recurse -Force
-      New-Item $DotfilesFolder -ItemType directory
+    Write-Host "Der Dotfiles-Ordner existiert bereits und ist nicht leer. Wählen Sie eine Option:"
+    Write-Host "1: Überschreiben"
+    Write-Host "2: Nicht überschreiben und existierendes Install-Skript starten"
+    Write-Host "3: Abbrechen"
+    $UserSelection = Read-Host "Bitte geben Sie die Nummer Ihrer Wahl ein"
+
+    switch ($UserSelection) {
+      "1" {
+        Remove-Item -Path $DotfilesFolder -Recurse -Force
+        New-Item $DotfilesFolder -ItemType Directory
+        # Fortfahren mit dem Download und der Installation
+      }
+      "2" {
+        # Pfad zum existierenden Install-Skript
+        $InstallScriptPath = Join-Path -Path $DotfilesWorkFolder -ChildPath "Install.ps1"
+        if (Test-Path $InstallScriptPath) {
+          Invoke-Expression $InstallScriptPath
+        } else {
+          Write-Host "Install-Skript nicht gefunden." -ForegroundColor Red
+        }
+        exit
+      }
+      "3" {
+        Write-Host "Vorgang durch den Benutzer abgebrochen." -ForegroundColor Red
+        exit
+      }
+      default {
+        Write-Host "Ungültige Auswahl. Bitte starten Sie das Skript erneut und wählen Sie eine gültige Option." -ForegroundColor Yellow
+        exit
+      }
     }
   }
 } else {
-  New-Item $DotfilesFolder -ItemType directory
+  New-Item $DotfilesFolder -ItemType Directory
 }
 
-# Download Dotfiles repository as Zip
+# Fortsetzung des Skripts für den Fall, dass Option 1 gewählt wurde...
 Try {
   Invoke-WebRequest -Uri $GitHubRepositoryUri -OutFile $ZipRepositoryFile
   $DownloadResult = $TRUE
-}
-catch [System.Net.WebException] {
+  # Weitere Logik für den Download und die Installation...
+} Catch [System.Net.WebException] {
   Write-Host "Fehler beim Herunterladen des Dotfiles-Repositorys" -ForegroundColor Red
+} Catch {
+  Write-Host "Ein unbekannter Fehler ist aufgetreten" -ForegroundColor Red
 }
 
 if ($DownloadResult) {
-  Add-Type -AssemblyName System.IO.Compression.FileSystem
-  [System.IO.Compression.ZipFile]::ExtractToDirectory($ZipRepositoryFile, $DotfilesFolder)
-  Remove-Item -Path $ZipRepositoryFile -Force
   Invoke-Expression (Join-Path -Path $DotfilesWorkFolder -ChildPath "Install.ps1")
 }
